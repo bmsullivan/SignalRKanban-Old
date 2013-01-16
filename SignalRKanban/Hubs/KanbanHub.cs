08f6764ab6730cf53f951c1b5d5f3a83d987f657
Brian Sullivan <bmsullivan@gmail.com>
2013-01-16 16:05:54 -0600

    Create card working

---
+++ SignalRKanban/Hubs/KanbanHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRKanban.Hubs
+{
+    using System.Collections.Concurrent;
+
+    using Microsoft.AspNet.SignalR;
+
+    using SignalRKanban.Models;
+
+    public class KanbanHub : Hub
+    {
+        private static ConcurrentDictionary<Guid, Card> _cards = new ConcurrentDictionary<Guid, Card>();
+
+        public void CreateCard()
+        {
+            var id = Guid.NewGuid();
+            var card = new Card { ID = id, Lane = "1", Content = ""};
+            _cards[id] = card;
+            Clients.All.cardCreated(card);                        
+        }
+    }
+}
--- SignalRKanban/Scripts/kanban.js
+++ SignalRKanban/Scripts/kanban.js
@@ -1,7 +1,17 @@
 ﻿$(function () {
-
+    var kanbanHub = $.connection.kanbanHub;
+    
+    KanbanBoard.prototype.createCard = function() {
+        kanbanHub.server.createCard();
+    };
+    
     var vm = new KanbanBoard();
     
     ko.applyBindings(vm);
 
+    kanbanHub.client.cardCreated = function (card) {
+        vm.addNewCardToLane(card.ID, card.Content, card.Lane);
+    };
+
+    $.connection.hub.start();
 });
